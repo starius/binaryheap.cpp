72892df1a1ad46714f43f7462cdca6457316c126
Boris Nagaev <bnagaev@gmail.com>
2015-05-20 02:12:19 +0300

    add test-string-payload.cpp

--- Makefile
+++ Makefile
@@ -1,5 +1,9 @@
-test: test-sort.exe
+test: test-sort.exe test-string-payload.exe
 	./test-sort.exe
+	./test-string-payload.exe
 
 test-sort.exe: test-sort.cpp binaryheap.hpp
+	$(CXX) -g -o $@ $<
+
+test-string-payload.exe: test-string-payload.cpp binaryheap.hpp
 	$(CXX) -g -o $@ $<
---
+++ test-string-payload.cpp
@@ -0,0 +1,64 @@
+#include <assert.h>
+#include <string>
+
+#include "binaryheap.hpp"
+
+int main() {
+    BinaryHeap<std::string, int> heap;
+    heap.insert("pos08", 98); // 1
+    heap.insert("pos05", 28); // 2
+    heap.insert("pos06", 36); // 3
+    heap.insert("pos09", 48); // 4
+    heap.insert("pos10", 68); // 5
+    heap.insert("pos13", 58); // 6
+    heap.insert("pos15", 80); // 7
+    heap.insert("pos04", 46); // 8
+    heap.insert("pos03", 19); // 9
+    heap.insert("pos11", 66); // 10
+    heap.insert("pos02", 22); // 11
+    heap.insert("pos12", 60); // 12
+    heap.insert("pos01", 15); // 13
+    heap.insert("pos14", 83); // 14
+    heap.insert("pos07", 59); // 15
+    heap.check();
+    //
+    assert(!heap.empty());
+    assert(heap.size() == 15);
+    assert(heap.valueByPayload("pos07") == 59);
+    std::string payload;
+    int value;
+    heap.top(payload, value); heap.pop();
+    assert(payload == "pos01" && value == 15);
+    heap.top(payload, value); heap.pop();
+    assert(payload == "pos03" && value == 19);
+    heap.top(payload, value); heap.pop();
+    assert(payload == "pos02" && value == 22);
+    heap.top(payload, value); heap.pop();
+    assert(payload == "pos05" && value == 28);
+    heap.top(payload, value); heap.pop();
+    assert(payload == "pos06" && value == 36);
+    heap.top(payload, value); heap.pop();
+    assert(payload == "pos04" && value == 46);
+    heap.top(payload, value); heap.pop();
+    assert(payload == "pos09" && value == 48);
+    heap.top(payload, value); heap.pop();
+    assert(payload == "pos13" && value == 58);
+    heap.top(payload, value); heap.pop();
+    assert(payload == "pos07" && value == 59);
+    heap.top(payload, value); heap.pop();
+    assert(payload == "pos12" && value == 60);
+    heap.top(payload, value); heap.pop();
+    assert(payload == "pos11" && value == 66);
+    heap.top(payload, value); heap.pop();
+    assert(payload == "pos10" && value == 68);
+    heap.top(payload, value); heap.pop();
+    assert(payload == "pos15" && value == 80);
+    heap.top(payload, value); heap.pop();
+    assert(payload == "pos14" && value == 83);
+    heap.top(payload, value); heap.pop();
+    assert(payload == "pos08" && value == 98);
+    //
+    assert(heap.empty());
+    assert(heap.size() == 0);
+    return 0;
+}
